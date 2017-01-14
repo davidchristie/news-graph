174521fe65748094b89a3dadb7217d352165da18
David Christie <davidchristie.nz@gmail.com>
2017-01-15 11:52:03 +1300

    Get react up and running

---
+++ src/js/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Header</h1>
+      </header>
+    )
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  entry: './src/client.js',
+  entry: './src/js/client.js',
   output: {
     path: './docs',
     filename: 'client.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
